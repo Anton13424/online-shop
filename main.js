d3050313176c9da1d692d1fd65da44c2278546bd
Anton13424 <anton3shkin@gmail.com>
2024-10-05 12:45:48 +0300

    btn-price

--- main.js
+++ main.js
@@ -17,10 +17,39 @@ iPhone.addEventListener('click', function(event) {
     currentPrice = clickTarget.getAttribute('data-size')
     console.log(currentPrice)
     if (currentPrice === 'medium'){
-        priceItem.textContent = 'цена 800000р.'
+        priceItem.textContent = 'цена 800 000р.'
     }
     if (currentPrice === 'large'){
-        priceItem.textContent = 'цена 10000000 руб.'
+        priceItem.textContent = 'цена 10 000 000 руб.'
+    }
+})
+const samsung = document.querySelector('.samsung')
+samsung.addEventListener('click', function(event) {
+    let clickTarget = event.target
+    let activeButton = samsung.querySelector('.active')
+    let priceItem = samsung.querySelector('.price')
+    let currentPrice
+    let activeButtonColor = samsung.querySelector('.active-border')
+
+    if (clickTarget.classList.contains('color-btn') && !clickTarget.classList.contains('active-border')) {
+        clickTarget.classList.add('active-border')
+        activeButtonColor.classList.remove('active-border')
+    }
+    if (clickTarget.classList.contains('btn') && !clickTarget.classList.contains('active')){
+        clickTarget.classList.add('active')
+        activeButton.classList.remove('active')
+    }
+    currentPrice = clickTarget.getAttribute('data-size')
+    console.log(currentPrice)
+    if (currentPrice === 'medium'){
+        priceItem.textContent = 'цена 40 0000р.'
+    }
+    if (currentPrice === 'large'){
+        priceItem.textContent = 'цена 70 000 руб.'
+    }
+    if (clickTarget.classList.contains('buy-button')){
+        clickTarget.classList.remove('btn btn-primary')
+        clickTarget.classList.add('btn btn-secondary')
     }
 })
 
